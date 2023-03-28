19ffeb2d0864a9cfcfad6499a234d74f974a27fc
Q <foldervapp@gmail.com>
2023-03-28 22:09:07 +0800

    up

--- fgchecker-sample/src/main/java/com/rvalerio/foregroundappchecker/ForegroundToastService.java
+++ fgchecker-sample/src/main/java/com/rvalerio/foregroundappchecker/ForegroundToastService.java
@@ -1,6 +1,7 @@
 package com.rvalerio.foregroundappchecker;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -42,6 +43,7 @@ public class ForegroundToastService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        initDefaultNotificationChannel(this);
         registerReceivers();
         startChecker();
         createStickyNotification();
@@ -93,16 +95,31 @@ public class ForegroundToastService extends Service {
         unregisterReceiver(stopServiceReceiver);
     }
 
+    public static final String CHANNEL_DEFAULT_ID = "channelDefault";
+    public static final String CHANNEL_DEFAULT_NAME = "Default";
+    public static final String CHANNEL_DEFAULT_DESCRIPTION = "Default channel";
+    public void initDefaultNotificationChannel(Context context) {
+        if (context == null) {
+            return;
+        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_DEFAULT_ID, CHANNEL_DEFAULT_NAME, NotificationManager.IMPORTANCE_LOW);
+            notificationChannel.setDescription(CHANNEL_DEFAULT_DESCRIPTION);
+            NotificationManager mManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
     private Notification createStickyNotification() {
         NotificationManager manager = ((NotificationManager) getSystemService(NOTIFICATION_SERVICE));
-        Notification notification = new NotificationCompat.Builder(this)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_DEFAULT_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setOngoing(true)
                 .setOnlyAlertOnce(true)
                 .setAutoCancel(false)
                 .setContentTitle(getString(R.string.app_name))
                 .setContentText(getString(R.string.stop_service))
-                .setContentIntent(PendingIntent.getBroadcast(this, 0, new Intent(STOP_SERVICE), PendingIntent.FLAG_UPDATE_CURRENT))
+                .setContentIntent(PendingIntent.getBroadcast(this, 0, new Intent(STOP_SERVICE), PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT))
                 .setWhen(0)
                 .build();
         manager.notify(NOTIFICATION_ID, notification);
